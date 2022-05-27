f41a586342a70ef7149cfab4bb9eaefb7ed03c4b
Bryant Huynh <bryant.huynh03@gmail.com>
2022-05-26 19:11:45 -0500

    day3

--- index.js
+++ index.js
@@ -46,30 +46,34 @@ if (dayOfWeek == 0) {
     dayOfWeekAlpha= "Saturday";
 }
 
+function showMonths() {
+  document.write ( 'hello' );
+}
+
 function checkCurrMonth() {
     if ( _i == 1) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Jan'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" onclick="showMonths()"data-value="'+_i+'"'+_addClass+'>'+'Jan'+'</button></li>' );
       } else if ( _i == 2) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Feb'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Feb'+'</button></li>' );
       } else if ( _i == 3) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Mar'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Mar'+'</button></li>' );
       } else if ( _i == 4) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Apr'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Apr'+'</button></li>' );
       } else if ( _i == 5) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'May'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'May'+'</a></li>' );
       } else if ( _i == 6) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Jun'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Jun'+'</a></li>' );
       } else if ( _i == 7) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Jul'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Jul'+'</a></li>' );
       } else if ( _i == 8) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Aug'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Aug'+'</a></li>' );
       } else if ( _i == 9) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Sep'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Sep'+'</a></li>' );
       } else if ( _i == 10) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Oct'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Oct'+'</a></li>' );
       } else if ( _i == 11) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Nov'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Nov'+'</a></li>' );
       } else if ( _i == 12) {
-        document.write( '<li><a href="#" title="'+_i+'" data-value="'+_i+'"'+_addClass+'>'+'Dec'+'</a></li>' );
+        document.write( '<li><button class="monthsButton" data-value="'+_i+'"'+_addClass+'>'+'Dec'+'</a></li>' );
       } 
 }
